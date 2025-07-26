8b6f72923db4a905be59dc60cceddeb2b422a178
123eslam <eslameltamalawy124@gmail.com>
2025-07-26 21:55:45 +0300

    Add in-memory database

---
+++ OrderManagementSystemTask.DAL/Presistance/Data/OrderManagementDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OrderManagementSystemTask.DAL.Presistance.Data
+{
+    public class OrderManagementDbContext : DbContext
+    {
+        public OrderManagementDbContext(DbContextOptions<OrderManagementDbContext> options)
+             : base(options)
+        {
+        }
+    }
+}
--- OrderManagementSystemTask.PL/Program.cs
+++ OrderManagementSystemTask.PL/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using OrderManagementSystemTask.DAL.Presistance.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Configure InMemory Database
+builder.Services.AddDbContext<OrderManagementDbContext>(options =>
+    options.UseInMemoryDatabase("OrderManagementDb"));
 
 var app = builder.Build();
 
